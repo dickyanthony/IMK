da7390f986b6ace46e80efa270a5cb8735fcbbd3
dickyanthony <dickyanthony0812@gmail.com>
2022-12-05 11:12:49 +0700

    [DA] - Add Content BAB 1

--- src/screens/Bab1/index.js
+++ src/screens/Bab1/index.js
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./imk.css";
 import Bab1 from "./Bab1";
+import Bab11 from "./Bab11";
+import Bab12 from "./Bab12";
+import Bab13 from "./Bab13";
+import Bab14 from "./Bab14";
 
 function BabFirst() {
   return (
     <div className="container">
-      <Bab1 />
+      <div className="contentContainer">
+        <Bab1 />
+        <Bab11 />
+        <Bab12 />
+        <Bab13 />
+        <Bab14 />
+      </div>
     </div>
   );
 }
