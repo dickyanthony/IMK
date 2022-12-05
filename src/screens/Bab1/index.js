f69a5416cea2c060fc9431e0305724bc6b9d2d84
dickyanthony <dickyanthony0812@gmail.com>
2022-12-05 11:34:42 +0700

    [DA] - Add dropdown

--- src/screens/Bab1/index.js
+++ src/screens/Bab1/index.js
@@ -6,9 +6,19 @@ import Bab12 from "./Bab12";
 import Bab13 from "./Bab13";
 import Bab14 from "./Bab14";
 import { PressButton } from "../../components/Button";
-
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 function BabFirst() {
+  const bab1Options = [
+    "Bab 1",
+    "1.1 ANTAR MUKA MANUSIA DAN KOMPUTER",
+    "1.2 INTERAKSI MANUSIA DAN KOMPUTER",
+    "1.3 PERANTI BANTU PENGEMBANG SISTEM",
+    "1.4 STRATEGI PENGEMBANGAN ANTARMUKA",
+  ];
   const [readFull, setReadFull] = useState(false);
+
+  const [defaultOption, setDefaultOption] = useState(bab1Options[0]);
   const ReadAll = () => {
     return (
       <>
@@ -20,14 +30,27 @@ function BabFirst() {
       </>
     );
   };
+
+  const ReadOne = () => {
+    return <Bab1 />;
+  };
   return (
     <div className="container">
+      {!readFull && (
+        <Dropdown
+          options={bab1Options}
+          onChange={(val) => setDefaultOption(val)}
+          value={defaultOption}
+          placeholder="Select an option"
+        />
+      )}
       <PressButton
         title={readFull ? "read less" : "read full"}
         onClick={() => setReadFull(!readFull)}
       />
+
       <div className="contentContainer">
-        {readFull ? <ReadAll /> : <div>asd</div>}
+        {readFull ? <ReadAll /> : <ReadOne />}
       </div>
     </div>
   );
