7a0f592254ddf486ab8d50b5df8195803a5359fe
dickyanthony <dickyanthony0812@gmail.com>
2022-12-05 11:26:00 +0700

    [DA] - Add content Bab 1

---
+++ src/screens/Bab1/Bab1.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./imk.css";
+
+export default function Bab1() {
+  return (
+    <div>
+      <h1>BAB I PENDAHULUAN</h1>
+      <div>
+        Istilah ramah dengan pengguna (user /friendy) dan WYSIWYG ( what you see
+        is what you get) sering digunakan ketika seseorang menawarkan sebuah
+        perangkat lunak atau program aplikasi. Anda yang baru pertama kali
+        mendengar istilah di atas barangkali akan bingung, tidak mengerti akan
+        arti dari kedua istilah tersebut. Tetapi, sejalan dengan pengalaman Anda
+        mengoperasikan perangkat lunak atau program aplikasi, secara tidak Anda
+        sadari istilah-istilah di atas akan merasuk ke dalam kumpulan kosa kata
+        yang telah Anda miliki sebelumnya. Pertanyaan yang timbul adalah:
+        Sebenarnya apa yang dimaksud dengan istilah ramah dengan pengguna,
+        WYSIWYG, dan beberapa istilah "serem" yang Iain? Istilah "ramah dengan
+        pengguna" digunakan untuk merujuk kepada karakteristik yang dimiliki
+        oleh perangkat lunak atau program aplikasi yang mudah dioperasikan.
+        Istilah di atas juga menyangkut sejumlah kemampuan Iain yang membuat
+        pengguna merasa betah dalam mengoperasikan program tersebut. Bahkan,
+        jika sebuah program mempunyai karakteristik "ramah dengan pengguna",
+        seorang pemula sekalipun tidak akan banyak mengalami kesulitan untuk
+        mengoperasikannya. Istilah inilah yang sering digunakan sebagai senjata
+        andalan bagi para penjual program aplikasi dalam memasarkan produknya,
+        dan menjadi salah satu kriteria utama bagi pengembang program. Tetapi di
+        Sisi Iain, penggunapun juga sering mensyaratkan agar program aplikasi
+        yang dia gunakan, selain dapat membantu untuk menyelesaikan suatu
+        pekerjaan dengan hasil yang sesuai keinginan, juga menyediakan berbagai
+        kemudahan pada saat dia mengoperasikan program yang dimaksud. Dengan
+        demikian tuntutan bahwa sebuah program aplikasi harus bersifat ramah
+        sesungguhnya juga datang dari pengguna.
+      </div>
+    </div>
+  );
+}
---
+++ src/screens/Bab1/Bab12.js
@@ -0,0 +1,136 @@
+import React from "react";
+import "./imk.css";
+
+export default function Bab12() {
+  return (
+    <div>
+      <h1>1.2 INTERAKSI MANUSIA DAN KOMPUTER</h1>
+      <div>
+        Interaksi Manusia-Komputer (untuk seterusnya disingkat dengan IMK)
+        adalah sebuah disiplin ilmu yang mempelajari perancangan, implementasi,
+        dan evaluasi sistem komputasi interaktif dan berbagai aspek terkait
+        (Hewett, et az, 1992, 1996). Dari perspektif ilmu kompüter, fokus İMK
+        adalah pada interaksi, khususnya interaksi antara satu atau lebih
+        manusia (sebagai pengguna komputer) dengan satu atau lebih meşin
+        komputasi (komputer). Situasİ klasik yang sering kita jumpai adalah
+        penggunaan program berbasis grafik yang interaktif. Dengan memperluas
+        pengertian tentang interaksi manusia dan meşin akan membawa kita kepada
+        topik yang lebih luas, yang tidak mungkin dipisahkan dari IMK, antara
+        lain topik tentang peranti maşukan/keluaran, dan lingkungan kerja.
+        Istilah "interaksi manusia dan kompüter” atau "interaksi manusia dan
+        meşin” melingkupi dua sisi, yaitu meşin dan manusia. Pertama kita perlu
+        melihat istilah "meşin”. istilah meşin lebih populer dengan sebutan
+        kompüter. Berbagai jenis kompüter yang kita kenal antara lain adalah
+        mainframe, workstation, dan komputer pribadi. Komputer pribadi biasanya
+        dalam bentuk komputer meja, atau komputer jinjing/pangku Selain berbagai
+        jenis di atas, komputer juga muncul dalam bentuk mesin komputasi
+        terpadu, misalnya sebagai bagian dari mesin cuci, kokPit pesawat
+        terbang, atau pemanggang microwave. Dengan demikian teknik untuk
+        merancang antarmuka pada komputer dapat juga digunakan untuk merancang
+        antarmuka pada mesin-mesin terpadu seperti disebutkan di atas. Tetapi
+        jika kita mengabaikan aspek komputasi dan interaksi sebuah mesin, dan
+        memperlakukan perancangan mesin yang bersifat mekanis dan pasif,
+        misalnya perancangan sebuah cangkul, maka kita tidak ak.an menganggap
+        hal itu sebagai bagian dari IM K. Hubungan ini lebih Umum disebut
+        sebagai human factor yang bersifat umum, yang mempelajari aspek manusia
+        dari semua peranti, tetapi bukan dari Sisi mekanismenya. Sebaliknya, IMK
+        mempelajari sisi mekanisme dan manusia, tetapi pada kelompok peranti
+        yang lebih sempit. Berikutnya kita lihat aspek "manusia”. Jika kita
+        melihat "manusia” sebagai sekelompok orang atau sebuah organisasi, maka
+        antarmuka di sini termasuk di dalamnya antara lain sistem terdistribusi,
+        komunikasi antar manusia terbantu komputer, atau suatu pekerjaan yang
+        secara kooperatif dikerjakan oleh sekelompok orang yang menggunakan
+        bantuan sistem komputer. Kesemuanya ini merupakan topik penting dalam
+        ruang lingkup IM K. Jika kita melangkah lebih jauh dengan
+        mempertimbangkan pekerjaan perancangan dari sisi pandang asal-usul
+        pekerjaan dan kepuasan manusia, maka komputer tidak akan selalu muncul,
+        dan IMK hanya merupakan salah satu pendukung, di samping pendukung yang
+        lain. Selain sudut pandang di atas, terdapat beberapa sudut pandang lain
+        yang akan menempatkan fokus IMK secara berbeda dibandingkan ilmu
+        komputer, seperti halnya fokus tentang basis data Yang berbeda antara
+        sudut pandang ilmu komputer dengan sudut pandang bisnis. IMK merupakan
+        bidang antar disiplin ilmu, dan masing-masing disiplin ilmu memberi
+        penekanan pada aspek yang berbeda. Beberapa disiplin ilmu tersebut
+        antara lain (Downtown, 1992): Ilmu komputer membahas tentang perancangan
+        aplikasi dan rekayasa/perancangan Antarmuka untuk dapat di gunakan oleh
+        manusia dengan mudah. dałam hal ini menyangkut pemilihan program bantu
+        pendukung, bahasa pemrograman, teknik pe-mrograman, serta bidang ilmu
+        komputer lain seperti pengolahan bahasa alami, struktur data, basis
+        data, dan lain-lain. psikologi: membahas tentang penerapan teori proses
+        kognitif dan analisis empiris tentang perilaku pengguna. di atas telah
+        disebutkan bahwa kita selalu berharap agar program aplikasi yang kita
+        susun dapat dimanfaatkan oleh pengguna lain. setiap pengguna mempunyai
+        sifat yang berbeda satu sama lain sehingga perancang sistem interaksi
+        manusia-komputer juga harus mempelajari aspek psikologi pengguna untuk
+        dapat memahami bagaimana pengguna dapat menggunakan sifat dan kebiasaan
+        baiknya, menggunakan persepsi dan pengolahan kognitif serta ketrampilan
+        motorik yang dimilikinya agar dapat menjodohkan mesin dengan manusia
+        untuk mendapatkan kerja sama yang serasi. psikologi eksperimental
+        menyediakan dasar teknik evaluasi formal untuk mengukur unjuk kerja dan
+        opini terhadap sistem manusia-komputer. Antropologi dan sosiologi:
+        membahas tentang interaksi antara teknologi, kerja, dan organisasi.
+        seperti diketahui, interaksi sangat dipengaruhi oleh teknologi yang
+        digunakan (misalnya dałam sebuah kantor). di sisi lain, antropologi juga
+        dapat memberikan pandangan mendalam tentang cara kerja berkelompok yang
+        masing- masing anggotanya diharapkan dapat memberikan kontribusi sesuai
+        dengan bidang masing-masing. Sosiologi berkaitan dengan studi tentang
+        pengaruh sistem manusiakomputer dałam struktur sosial. adanya
+        kekhawatiran sementara orang tentang akan di-phk-nya mereka atau
+        disingkirkannya mereka dari kantor mereka karena adanya otomasi kantor
+        sering menjadi bahan yang menarik untuk didiskusikan. Perancangan grafis
+        dan tipografi Ada kata bijak yang mengatakan bahwa "Sebuah gambar dapat
+        bermakna sama dengan seribu kata”. Dałam dunia komputer, kata ini dapat
+        diartikan bahwa gambar dapat digunakan sebagai sarana dialog yang
+        efektif antara manusia dengan komputer. Keahlian merancang grafik dan
+        tipografi menjadi salah satu kunci penting untuk menunjang keberhasilan
+        sisten) komputer. katena antatanuka yang disusun dapat menjadi lebih
+        luwes dan ampuh. Teknik elektronika. Berbicara tentang komputer,
+        khususnya dari perangkat keras. pastilah tidak terlepas dari pembicaraan
+        tentang teknik elektronika. Dalam bidang inilah kita (lapat melnpelajari
+        banyak sekali aspek yang berhubungan dengan perangkat keras komputer.
+        Ergonomik. Saat ini semakin banyak orang yang bekerja di depan terminal
+        komputer untuk jangka waktu yang cukup lama. Oleh sebab itu untuk
+        pengguna yang demikian perlu dipersiapkan meja kerja yang nyaman, untuk
+        menghindari kemungkinan adanya kecelakaan, baik ringan maupun berat.
+        Ergonomik berhubungan dengan aspek fisik untuk mendapatkan lingkungan
+        kerja yang nyaman. Bentuk fisik meja dan kursi kerja, layar tampilan,
+        bentuk papan ketik, posisi duduk, pengaturan lampu, kebersihan tempat
+        kerja, dan beberapa aspek lain sangat berpengaruh pada kenyamanan
+        lingkungan kerja. Meski sifat seorang pengguna dengan pengguna lain
+        berbeda, tetapi mereka pasti menginginkan adanya lingkungan kerja yang
+        nyaman ketika mereka bekerja dengan komputer. Linguistik. Pada saat kita
+        menggunakan komputer, seolah-olah kita sedang melakukan dialog dengan
+        komputer yang ada di hadapan kita Untuk dapat melakukan dialog, kita
+        memerlukan sarana komunikasi yang memadai. Sarana komunikasi ini
+        berbentuk suatu bahasa khusus, misalnya bahasa grafis, bahasa alami,
+        bahasa menu' ataupun bahasa perintah. Linguistik merupakan cabang ilmu
+        pengetahuan yang mempelajari bahasa. Beberapa aspek seperti komputasi
+        linguistik dan teori bahasa formal membentuk bidang khusus dalam ilmu
+        komputer. Sarana komunikasi inilah yang akan mengarahkan pengguna Ketika
+        ia berurusan dengan komputer. perancangan Industri. membahas tentang
+        produk-produk interaktif. Dengan semakin berkembangnya teknologi layar
+        tampilan, penggunaan layar sentuh semakin popular. Penggunaan layer
+        sentuh, beserta teknologi di belakang nya, telah menjadi bahan kajian
+        yang menarik. Saat ini, selain layar sentuh, layar sentuh banyak
+        (multitouch screen) juga semakin banyak digunakan. Lebih lanjut, dari
+        sudut pandang ilmu komputer, disiplin ilmu yang disebutkan di atas dapat
+        menjadi semacam pendukung IMK, seperti halnya fisika menjadi pendukung
+        disiplin teknik sipil, atau teknik mesin yang menjadi pendukung
+        robotika. Pelajaran yang dapat dipetik dari disiplin keteknikan adalah
+        bahwa perancangan selalu mempunyai konteks, dan optimisasi yang terlalu
+        sempit dari satu bagian dalam perancangan tersebut akan dianggap tidak
+        sah pada konteks yang lebih luas. Dengan demikian, dari sudut pandang
+        ilmu komputer secara langsung, adalah perlu untuk membahas IMK secara
+        luas untuk memberikan bekal kepada mahasiswa (dan praktisi) untuk
+        menghindari kelemahan klasik yang memisahkan antara rancangan dengan
+        konteks. Selain itu perlu juga diketahui bahwa IMK berurusan dengan
+        kinerja gabungan antara manusia dan mesin; struktur komunikasi antara
+        manusia dan mesin, kemampuan manusia menggunakan mesin (termasuk
+        kemampuan untuk dipelajari//earnability dari antarmuka yang digunakan);
+        algoritma dan pemrograman antarmuka; isu-isu rekayasa yang muncul pada
+        saat merancang dan membangun antarmuka; proses spesifikasi, perancangan
+        dan implementasi antarmuka; serta biaya perancangan.
+      </div>
+    </div>
+  );
+}
