30a6dd57266acb115136069373db30cf3c48c79f
dickyanthony <dickyanthony0812@gmail.com>
2022-12-06 18:26:32 +0700

    [DA] - fix footer responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,48 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../../assets/logo-header.png";
 import { NavLink } from "react-router-dom";
-import { headerItems } from "./HeaderItem";
-import Dropdown from "../Dropdown";
 import "./header.css";
 
 function Header() {
-  const [dropdown, setDropdown] = useState(false);
-
   return (
-    <>
-      <div className="navbar_products">
-        <div className="secondary_container">
-          <img className="navprod_logo" src={logo} alt={logo} />
+    <div className="navbar_products">
+      <div className="secondary_container">
+        <img className="navprod_logo" src={logo} alt={logo} />
 
-          <div className="navprod_link">
-            <div>
-              <NavLink
-                to={"/"}
-                // className={({ isActive }) =>
-                //   isActive ? "asdsd" : "navprod_text"
-                // }
-                className={({ isActive }) =>
-                  isActive ? "navprod_text_active" : "navprod_text"
-                }
-              >
-                Home
-              </NavLink>
-            </div>
+        <div className="navprod_link">
+          <div>
+            <NavLink
+              to={"/"}
+              // className={({ isActive }) =>
+              //   isActive ? "asdsd" : "navprod_text"
+              // }
+              className={({ isActive }) =>
+                isActive ? "navprod_text_active" : "navprod_text"
+              }
+            >
+              Home
+            </NavLink>
+          </div>
 
-            <div>
-              <NavLink
-                to="/About"
-                className={({ isActive }) =>
-                  isActive ? "navprod_text_active" : "navprod_text"
-                }
-              >
-                About
-              </NavLink>
-            </div>
+          <div>
+            <NavLink
+              to="/About"
+              className={({ isActive }) =>
+                isActive ? "navprod_text_active" : "navprod_text"
+              }
+            >
+              About
+            </NavLink>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
