feebcdff1220704c88fd4ee08a445f89933dacef
dickyanthony <dickyanthony0812@gmail.com>
2022-12-09 16:44:45 +0700

    [DA] - add search

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import { getWindowDimensions } from "../../constant";
 import logo from "../../assets/logo-header.png";
+import respLogo from "../../assets/respLogo.png";
 import { NavLink } from "react-router-dom";
+import { Menu, Transition } from "@headlessui/react";
 import "./header.css";
-
+import SearchBar from "../Search/searchBar";
+import { book } from "../../constant";
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 function Header() {
-  return (
-    <div className="navbar_products">
-      <div className="secondary_container">
-        <img className="navprod_logo" src={logo} alt={logo} />
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
 
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const RenderBigHead = () => {
+    return (
+      <div className="secondary_container">
+        <img className="w-10 h-10 ml-10" src={respLogo} alt={logo} />
+        <h1 className="text-2xl font-bold text-[#000000] ">IMK</h1>
         <div className="navprod_link">
+          <div>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? "navprod_text_active" : "navprod_text"
+              }
+            >
+              Home
+            </NavLink>
+          </div>
+
+          <div>
+            <NavLink
+              to="/About"
+              className={({ isActive }) =>
+                isActive ? "navprod_text_active" : "navprod_text"
+              }
+            >
+              About
+            </NavLink>
+          </div>
+        </div>
+        <SearchBar placeholder="Cari" data={book} />
+      </div>
+    );
+  };
+  const RenderMediumHead = () => {
+    return (
+      <div
+        className="w-full h-20 flex justify-between items-center px-8 text-white"
+        style={{ boxShadow: "0px 5px 40px rgba(0, 0, 0, 0.15)" }}
+      >
+        <img className="w-10 h-10 ml-10" src={respLogo} alt={logo} />
+
+        <div className="navprod_link flex justify-center">
           <div>
             <NavLink
               to={"/"}
@@ -35,8 +90,98 @@ function Header() {
             </NavLink>
           </div>
         </div>
+        <SearchBar placeholder="Cari" data={book} />
       </div>
-    </div>
+    );
+  };
+
+  const RenderSmallHead = () => {
+    return (
+      <div
+        className="w-full h-20 flex justify-between items-center px-8 text-white"
+        style={{ boxShadow: "0px 5px 40px rgba(0, 0, 0, 0.15)" }}
+      >
+        <img className="w-10 h-10 ml-10" src={respLogo} alt={logo} />
+        <h1 className="text-2xl font-bold text-[#000000] ml-20 mr-5">IMK</h1>
+        <ul className="flex items-center">
+          <li className="p-4"></li>
+          <li className="p-4">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button>
+                  <div class="relative flex items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-black ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+                    <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 group-focus:-rotate-[45deg] origin-center">
+                      <div class="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75 group-focus:-translate-y-[1px]"></div>
+                      <div class="bg-white h-[1px] rounded"></div>
+                      <div class="bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75 group-focus:translate-y-[1px]"></div>
+                    </div>
+                  </div>
+                </Menu.Button>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-5"
+                enterFrom="transform opacity-0 "
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-25"
+                leaveFrom="transform opacity-100 "
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+                  <div className="py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <NavLink
+                          to="/"
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          Home
+                        </NavLink>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <NavLink
+                          to="About"
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          About
+                        </NavLink>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {windowDimensions.width >= 885 ? (
+        <div className="navbar_products flex w-full justify-center">
+          <RenderBigHead />
+        </div>
+      ) : windowDimensions.width >= 662 ? (
+        <RenderMediumHead />
+      ) : (
+        <RenderSmallHead />
+      )}
+    </>
   );
 }
 
---
+++ src/components/Search/searchBar.js
@@ -0,0 +1,116 @@
+import React from "react";
+import "./searchbar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Turnstone from "turnstone";
+import { book } from "../../constant";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+export default function SearchBar() {
+  const [hasFocus, setHasFocus] = useState(false);
+  const [newLink, setNewLink] = useState("");
+  const tes = useRef();
+  const navigate = useNavigate();
+  const styles = {
+    input:
+      "w-full h-12 border border-crystal-400 py-2 pl-10 pr-9 text-xl text-oldsilver-800 outline-none rounded-md",
+    inputFocus:
+      "w-full h-12 border-x-0 border-t-0 border-b border-crystal-500 py-2 pl-10 pr-9 text-xl text-oldsilver-800 outline-none sm:rounded-md sm:border",
+    query: "text-oldsilver-800 placeholder-oldsilver-400",
+    typeahead: "text-oldsilver-400 border-white",
+    cancelButton: `absolute w-10 h-12 inset-y-0 left-0 items-center justify-center z-10 text-crystal-600 inline-flex sm:hidden`,
+    clearButton:
+      "absolute inset-y-0 right-0 w-10 inline-flex items-center justify-center text-crystal-500 hover:text-hotpink-300",
+    listbox:
+      "w-full bg-white sm:border sm:border-crystal-500 sm:rounded-md text-left sm:mt-2 p-2 sm:drop-shadow-xl",
+    groupHeading:
+      "cursor-default mt-2 mb-0.5 px-1.5 uppercase text-sm text-hotpink-500",
+    item: "cursor-pointer p-1.5 text-lg whitespace-nowrap text-ellipsis overflow-hidden text-oldsilver-700",
+    highlightedItem:
+      "cursor-pointer p-1.5 text-lg whitespace-nowrap sm:text-ellipsis overflow-hidden text-oldsilver-700 rounded-md bg-gradient-to-t from-crystal-100 to-white",
+    noItems: "cursor-default text-center my-20",
+  };
+  useEffect(() => {
+    setNewLink("");
+    tes.current.clear();
+    tes.current?.query("");
+    setHasFocus(false);
+  });
+
+  useEffect(() => {
+    if (newLink) {
+      navigate(newLink);
+      tes.current.clear();
+      tes.current?.query("");
+      tes.current?.focus(false);
+      tes.current.select();
+    }
+  }, [newLink]);
+  function _onNavigate(link) {
+    if (link) {
+      setNewLink(link.onClick);
+    }
+  }
+
+  const listbox = [
+    {
+      name: "Materi",
+      data: book,
+      searchType: "startswith",
+      displayField: "name",
+    },
+  ];
+  const onBlur = () => setHasFocus(false);
+  const onFocus = () => setHasFocus(true);
+
+  const containerStyles = hasFocus
+    ? "fixed block w-full h-full top-0 left-0 bg-white z-50 overflow-auto sm:relative sm:w-4/12 sm:h-auto sm:top-auto sm:left-auto sm:bg-transparent sm:z-auto sm:overflow-visible"
+    : "relative w-11/12 sm:w-4/12";
+
+  const iconDisplayStyle = hasFocus
+    ? "hidden text-crystal-600"
+    : "inline-flex text-crystal-500";
+  const Item = ({ item }) => {
+    return (
+      <div key={item.id} className="flex items-center cursor-pointer px-5 py-4">
+        <img
+          width={35}
+          height={35}
+          src={item.img}
+          alt={item.name}
+          className="mr-5"
+          // onClick={() => console.log("click==>", item.onClick)}
+        />
+        <p>{item.name}</p>
+      </div>
+    );
+  };
+  return (
+    <div className={containerStyles}>
+      <span
+        className={`absolute w-10 h-12 inset-y-0 left-0 items-center justify-center z-10 sm:inline-flex ${iconDisplayStyle}`}
+      >
+        <SearchIcon className="w-6 h-6" />
+      </span>
+      <Turnstone
+        id="Materi"
+        ref={tes}
+        cancelButton={true}
+        clearButton={true}
+        listbox={listbox}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        autoFocus={true}
+        matchText={false}
+        placeholder="Cari materi"
+        noItemsMessage="Tidak ada materi"
+        styles={styles}
+        Item={Item}
+        onSelect={(item) => _onNavigate(item)}
+      />
+      {/* <button onClick={handleQuery}>Perform Query</button> */}
+    </div>
+  );
+}
